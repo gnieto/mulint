4b8af7c5b41b6498095730bceb51ccf00be78b3b
Guillem Nieto <gnieto.talo@gmail.com>
2018-02-17 23:29:29 +0100

    UI test

---
+++ ui_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+current_out=$(mktemp)
+expected_out=$(mktemp)
+go run main.go ./tests | sort  > $current_out
+cat tests/expected.stderr | sort > $expected_out
+
+diff $current_out $expected_out > /dev/null
